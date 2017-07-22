33c47a4f2e24ff3672ff1eeede7ffef6b9db5fa8
Chris Lewis <cl_myles@hotmail.com>
2017-07-22 22:31:44 +1000

    Added some suuuuuuuuper basic mouse-follow code

---
+++ Assets/Scripts/MouseFollower.cs
@@ -0,0 +1,28 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class MouseFollower : MonoBehaviour 
+{
+	[SerializeField]
+	private float SmoothTime = 0.5f;
+	[SerializeField]
+	private float MaxSpeed = 1f;
+
+	private Vector3 velocity;
+
+	// Use this for initialization
+	void Start () 
+	{
+		
+	}
+	
+	// Update is called once per frame
+	void Update () 
+	{
+		Vector3 targetPosition = Camera.main.ScreenToWorldPoint(Input.mousePosition);
+		targetPosition.z = 0;
+
+		transform.position = Vector3.SmoothDamp(transform.position, targetPosition, ref velocity, SmoothTime, MaxSpeed);
+	}
+}
