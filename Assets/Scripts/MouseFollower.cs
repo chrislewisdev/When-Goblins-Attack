9a9f2cd567f8c06ec9a7550488f82c583f886dd5
Chris Lewis <cl_myles@hotmail.com>
2017-07-22 23:50:34 +1000

    Uhhhh got some walk animations going and made some movement adjustments

--- Assets/Scripts/MouseFollower.cs
+++ Assets/Scripts/MouseFollower.cs
@@ -12,23 +12,43 @@ public class MouseFollower : MonoBehaviour
 	private float GoodEnoughRange = 5f;
 
 	private new Rigidbody2D rigidbody;
+	private Animator animator;
+	private SpriteRenderer sprite;
+
+	private Vector3 targetPosition;
 	private Vector3 velocity;
 
 	// Use this for initialization
 	void Start () 
 	{
 		rigidbody = GetComponent<Rigidbody2D>();
+		animator = GetComponent<Animator>();
+		sprite = GetComponent<SpriteRenderer>();
 	}
 	
 	// Update is called once per frame
 	void Update () 
 	{
-		Vector3 targetPosition = Camera.main.ScreenToWorldPoint(Input.mousePosition);
+		targetPosition = Camera.main.ScreenToWorldPoint(Input.mousePosition);
 		targetPosition.z = 0;
 
 		if ((transform.position - targetPosition).magnitude > GoodEnoughRange)
 		{
 			rigidbody.MovePosition(Vector3.SmoothDamp(transform.position, targetPosition, ref velocity, SmoothTime, MaxSpeed));
 		}
+		else
+		{
+			velocity = rigidbody.velocity;
+		}
+
+		sprite.flipX = velocity.x < 0;
+
+		animator.SetFloat("velocity", velocity.magnitude);
+	}
+
+	void OnDrawGizmos()
+	{
+		Gizmos.DrawLine(transform.position, targetPosition);
+		Gizmos.DrawWireSphere(targetPosition, GoodEnoughRange);
 	}
 }
