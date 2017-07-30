8bcd49561ecc313e8bd10b2f746cf92ba9de4d88
Chris Lewis <cl_myles@hotmail.com>
2017-07-30 14:34:27 +1000

    Got some basic camera movement working

---
+++ Assets/Scripts/CameraController.cs
@@ -0,0 +1,41 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CameraController : MonoBehaviour
+{
+	[SerializeField]
+	private float MaxMoveSpeed = 1f;
+	[SerializeField]
+	private Vector2 MouseBounds;
+	[SerializeField]
+	private bool DrawGizmos = true;
+
+    // Use this for initialization
+    void Start()
+    {
+
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+		Vector3 mousePosition = Camera.main.ScreenToWorldPoint(Input.mousePosition);
+		mousePosition.z = transform.position.z;
+
+		Vector3 deltaPosition = transform.position - mousePosition;
+		if (Mathf.Abs(deltaPosition.x) > MouseBounds.x / 2 || Mathf.Abs(deltaPosition.y) > MouseBounds.y / 2)
+		{
+			transform.position = Vector3.MoveTowards(transform.position, mousePosition, MaxMoveSpeed);
+		}
+    }
+
+	void OnDrawGizmos()
+	{
+		if (DrawGizmos)
+		{
+			Gizmos.color = Color.green;
+			Gizmos.DrawWireCube(transform.position, MouseBounds);
+		}
+	}
+}
