6d9192115391e84613b20896771b5cb725d8e353
Chris Lewis <cl_myles@hotmail.com>
2017-09-09 20:52:05 +1000

    Made a new axeman prefab that works using pathfinding

---
+++ Assets/Scripts/Movement/NavMeshMouseFollower.cs
@@ -0,0 +1,30 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.AI;
+
+public class NavMeshMouseFollower : MonoBehaviour
+{
+	private NavMeshAgent navMeshAgent;
+
+	void Start()
+	{
+		navMeshAgent = GetComponentInParent<NavMeshAgent>();
+
+		if (!navMeshAgent) Debug.LogWarning("No NavMeshAgent for NavMeshMouseFollower. This component needs to reside under a NavMeshAgent!");
+	}
+
+	void Update()
+	{
+		Vector3 targetPosition = GetTargetPosition();
+		targetPosition.z = 0;
+
+		if (navMeshAgent.isActiveAndEnabled) navMeshAgent.SetDestination(targetPosition);
+	}
+
+
+    private Vector3 GetTargetPosition()
+    {
+        return Camera.main.ScreenToWorldPoint(Input.mousePosition);;
+    }
+}
--- Assets/Scripts/Visuals/SpriteFlipper.cs
+++ Assets/Scripts/Visuals/SpriteFlipper.cs
@@ -1,6 +1,7 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.AI;
 
 public class SpriteFlipper : MonoBehaviour 
 {
@@ -8,6 +9,7 @@ public class SpriteFlipper : MonoBehaviour
 	private float ThresholdVelocity = 0.1f;
 
 	private new Rigidbody2D rigidbody;
+	private NavMeshAgent navMeshAgent;
 	private SpriteRenderer sprite;
 
 	// Use this for initialization
@@ -15,14 +17,20 @@ public class SpriteFlipper : MonoBehaviour
 	{
 		rigidbody = GetComponent<Rigidbody2D>();
 		sprite = GetComponent<SpriteRenderer>();
+		navMeshAgent = GetComponentInParent<NavMeshAgent>();
 	}
 	
 	// Update is called once per frame
 	void Update () 
 	{
-		if (Mathf.Abs(rigidbody.velocity.x) > ThresholdVelocity)
+		if (rigidbody && Mathf.Abs(rigidbody.velocity.x) > ThresholdVelocity)
 		{
 			sprite.flipX = rigidbody.velocity.x < 0;
+		}
+
+		if (navMeshAgent && Mathf.Abs(navMeshAgent.velocity.x) > ThresholdVelocity)
+		{
+			sprite.flipX = navMeshAgent.velocity.x < 0;
 		}
 	}
 }
